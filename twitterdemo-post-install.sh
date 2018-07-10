ed45d3ca53b7d747c8751cb65a882e1b9d9da402
Purnima Kuchikulla <purnima.kuchikulla@gmail.com>
2018-07-10 15:15:46 -0700

    Create twitterdemo-post-install.sh

---
+++ twitterdemo-post-install.sh
@@ -0,0 +1,38 @@
+exec > >(tee -i /root/demo-install.log)
+exec 2>&1
+
+export ROOT_PATH=~
+echo "*********************************ROOT PATH IS: $ROOT_PATH"
+
+
+export AMBARI_HOST=$(hostname -f)
+echo "*********************************AMABRI HOST IS: $AMBARI_HOST"
+
+export CLUSTER_NAME=$(curl -u admin:admin -X GET http://$AMBARI_HOST:8080/api/v1/clusters |grep cluster_name|grep -Po ': "(.+)'|grep -Po '[a-zA-Z0-9\-_!?.]+')
+
+if [[ -z $CLUSTER_NAME ]]; then
+        echo "Could not connect to Ambari Server. Please run the install script on the same host where Ambari Server is installed."
+        exit 1
+else
+       	echo "*********************************CLUSTER NAME IS: $CLUSTER_NAME"
+fi
+
+export VERSION=`hdp-select status hadoop-client | sed 's/hadoop-client - \([0-9]\.[0-9]\).*/\1/'`
+export INTVERSION=$(echo $VERSION*10 | bc | grep -Po '([0-9][0-9])')
+echo "*********************************HDP VERSION IS: $VERSION"
+
+export HADOOP_USER_NAME=hdfs
+echo "*********************************HADOOP_USER_NAME set to HDFS"
+
+echo "********************************* Capturing Service Endpoint in the Environment"
+captureEnvironment
+
+echo "********************************* Creating HDFS Directories"
+createHDFSDirectories
+
+echo "********************************* Creating Directories"
+createDirectories
+
+echo "********************************* Creating HIVE Tables"
+createHiveTables
+
