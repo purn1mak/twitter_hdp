a6892d9480e0683c945a06b4ebe7f3a048d999e5
Purnima Kuchikulla <purnima.kuchikulla@gmail.com>
2018-07-14 17:09:33 -0700

    Update hiveddl.sql

--- hiveddl.sql
+++ hiveddl.sql
@@ -1,69 +1,15 @@
 set hive.cli.errors.ignore=true;
+set hive.exec.dynamic.partition.mode=nonstrict;
+set hive.exec.dynamic.partition=true;
+set hive.enforce.bucketing = true;
+SET hive.exec.compress.output=true;
+SET avro.output.codec=snappy;
 
 CREATE DATABASE IF NOT EXISTS twitter;
 USE twitter;
 
-CREATE EXTERNAL TABLE summary_tweets
-PARTITIONED BY (YEAR INT, Month INT, Day INT)
-ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.avro.AvroSerDe'
-WITH SERDEPROPERTIES ('avro.schema.literal'='
-{
-    "namespace": "com.batchiq.samples.tweet",
-    "type": "record",
-    "name": "tweetsummary",
-    "fields": [
-        {"name": "id", "type": "long"},
-        {"name": "id_str", "type": "string"},
-        {"name": "created_at", "type": "string"},
-        {"name": "timestamp_ms", "type": "string"},
-        {"name": "lang", "type": "string"},
-        {"name": "text", "type": "string"},
-        
-        {"name": "user", "type": {
-            "type": "record",
-            "name": "User",
-            "fields": [
-                {"name": "id", "type": "long"},
-                {"name": "id_str", "type": "string"},
-                {"name": "name", "type": "string"},
-                {"name": "screen_name", "type": "string"},
-                {"name": "location", "type": ["null", "string"]},
-                {"name": "url", "type": ["null", "string"]},
-                {"name": "description", "type": ["null", "string"]},
-                {"name": "followers_count", "type": "long"},
-                {"name": "friends_count", "type": "long"}
-            ]
-        }},
-
-        {"name": "entities", "type": {
-            "type": "record",
-            "name": "Entities",
-            "fields": [
-                {"name": "hashtags", "type": {
-                    "type": "array", "items": {
-                        "type": "record",
-                        "name": "Hashtag",
-                        "fields": [
-                            {"name": "text", "type": "string"}
-                        ]
-                    }
-                }},
-                {"name": "user_mentions", "type": {
-                    "type": "array", "items": {
-                        "type": "record",
-                        "name": "UserMention",
-                        "fields": [
-                            {"name": "id", "type": "long"},
-                            {"name": "id_str", "type": "string"},
-                            {"name": "screen_name", "type": "string"},
-                            {"name": "name", "type": "string"}
-                        ]
-                    }
-                }}
-            ]
-        }}
-    ]
-}
-')
-STORED AS AVRO
-LOCATION 's3a://pkuchikulla/tweet-lake/summary/';
+CREATE EXTERNAL TABLE IF NOT EXISTS tweetsummary 
+(id BIGINT, id_str STRING, created_at STRING, 
+timestamp_ms STRING, lang STRING, text STRING) 
+STORED AS ORC
+location 'gs://pkgcpbucket/tweet-lake/summary/';
