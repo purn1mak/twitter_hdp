c9b157dfa71723b8643eed55742a4751c2a7e896
Purnima Kuchikulla <purnima.kuchikulla@gmail.com>
2018-07-17 23:42:49 -0700

    Update hiveddl.sql

--- hiveddl.sql
+++ hiveddl.sql
@@ -8,8 +8,20 @@ SET avro.output.codec=snappy;
 CREATE DATABASE IF NOT EXISTS twitter;
 USE twitter;
 
-CREATE EXTERNAL TABLE IF NOT EXISTS tweetsummary 
+//// Twitter Tables
+
+CREATE EXTERNAL TABLE IF NOT EXISTS tweetsummary_part 
 (id BIGINT, id_str STRING, created_at STRING, 
 timestamp_ms STRING, lang STRING, text STRING) 
+PARTITIONED BY (year string, month string, day string)
 STORED AS ORC
-location 'gs://pkhwx/tweet-lake/summary/';
+location 'gs://pkgcpbucket/tweet-lake/summary/';
+
+//select * from tweetsummary_part ;
+// nothing returns.
+
+ALTER TABLE tweetsummary_part ADD PARTITION (year='2018',month='07',day='13') 
+location 'gs://pkgcpbucket/tweet-lake/summary/2018/07/13';
+
+//select * from tweetsummary_part ;
+//select * from tweetsummary_part where year='2018'and month='07'and day='13';
