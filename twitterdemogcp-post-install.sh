d744b641ddaa4ef4a03adfb64aad344991bfb230
Purnima Kuchikulla <purnima.kuchikulla@gmail.com>
2018-09-06 14:16:17 -0700

    Create twitterdemogcp-post-install.sh

---
+++ twitterdemogcp-post-install.sh
@@ -0,0 +1,276 @@
+waitForAmbari () {
+       	# Wait for Ambari
+       	LOOPESCAPE="false"
+       	until [ "$LOOPESCAPE" == true ]; do
+        TASKSTATUS=$(curl -u admin:BadPass#1 -I -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME | grep -Po 'OK')
+        if [ "$TASKSTATUS" == OK ]; then
+                LOOPESCAPE="true"
+                TASKSTATUS="READY"
+        else
+               	AUTHSTATUS=$(curl -u admin:BadPass#1 -I -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME | grep HTTP | grep -Po '( [0-9]+)'| grep -Po '([0-9]+)')
+               	if [ "$AUTHSTATUS" == 403 ]; then
+               	echo "THE AMBARI PASSWORD IS NOT SET TO: admin"
+               	echo "RUN COMMAND: ambari-admin-password-reset, SET PASSWORD: BadPass#1"
+               	exit 403
+               	else
+                TASKSTATUS="PENDING"
+               	fi
+       	fi
+       	echo "Waiting for Ambari..."
+        echo "Ambari Status... " $TASKSTATUS
+        sleep 2
+       	done
+}
+
+serviceExists () {
+       	SERVICE=$1
+       	SERVICE_STATUS=$(curl -u admin:BadPass#1 -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/$SERVICE | grep '"status" : ' | grep -Po '([0-9]+)')
+
+       	if [ "$SERVICE_STATUS" == 404 ]; then
+       		echo 0
+       	else
+       		echo 1
+       	fi
+}
+
+getServiceStatus () {
+       	SERVICE=$1
+       	SERVICE_STATUS=$(curl -u admin:BadPass#1 -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/$SERVICE | grep '"state" :' | grep -Po '([A-Z]+)')
+
+       	echo $SERVICE_STATUS
+}
+
+waitForService () {
+       	# Ensure that Service is not in a transitional state
+       	SERVICE=$1
+       	SERVICE_STATUS=$(curl -u admin:BadPass#1 -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/$SERVICE | grep '"state" :' | grep -Po '([A-Z]+)')
+       	sleep 2
+       	echo "$SERVICE STATUS: $SERVICE_STATUS"
+       	LOOPESCAPE="false"
+       	if ! [[ "$SERVICE_STATUS" == STARTED || "$SERVICE_STATUS" == INSTALLED ]]; then
+        until [ "$LOOPESCAPE" == true ]; do
+                SERVICE_STATUS=$(curl -u admin:BadPass#1 -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/$SERVICE | grep '"state" :' | grep -Po '([A-Z]+)')
+            if [[ "$SERVICE_STATUS" == STARTED || "$SERVICE_STATUS" == INSTALLED ]]; then
+                LOOPESCAPE="true"
+            fi
+            echo "*********************************$SERVICE Status: $SERVICE_STATUS"
+            sleep 2
+        done
+       	fi
+}
+
+waitForServiceToStart () {
+       	# Ensure that Service is not in a transitional state
+       	SERVICE=$1
+       	SERVICE_STATUS=$(curl -u admin:BadPass#1 -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/$SERVICE | grep '"state" :' | grep -Po '([A-Z]+)')
+       	sleep 2
+       	echo "$SERVICE STATUS: $SERVICE_STATUS"
+       	LOOPESCAPE="false"
+       	if ! [[ "$SERVICE_STATUS" == STARTED ]]; then
+        	until [ "$LOOPESCAPE" == true ]; do
+                SERVICE_STATUS=$(curl -u admin:BadPass#1 -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/$SERVICE | grep '"state" :' | grep -Po '([A-Z]+)')
+            if [[ "$SERVICE_STATUS" == STARTED ]]; then
+                LOOPESCAPE="true"
+            fi
+            echo "*********************************$SERVICE Status: $SERVICE_STATUS"
+            sleep 2
+        done
+       	fi
+}
+
+stopService () {
+       	SERVICE=$1
+       	SERVICE_STATUS=$(getServiceStatus $SERVICE)
+       	echo "*********************************Stopping Service $SERVICE ..."
+       	if [ "$SERVICE_STATUS" == STARTED ]; then
+        TASKID=$(curl -u admin:BadPass#1 -H "X-Requested-By:ambari" -i -X PUT -d "{\"RequestInfo\": {\"context\": \"Stop $SERVICE\"}, \"ServiceInfo\": {\"maintenance_state\" : \"OFF\", \"state\": \"INSTALLED\"}}" http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/$SERVICE | grep "id" | grep -Po '([0-9]+)')
+
+        echo "*********************************Stop $SERVICE TaskID $TASKID"
+        sleep 2
+        LOOPESCAPE="false"
+        until [ "$LOOPESCAPE" == true ]; do
+            TASKSTATUS=$(curl -u admin:BadPass#1 -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/requests/$TASKID | grep "request_status" | grep -Po '([A-Z]+)')
+            if [ "$TASKSTATUS" == COMPLETED ]; then
+                LOOPESCAPE="true"
+            fi
+            echo "*********************************Stop $SERVICE Task Status $TASKSTATUS"
+            sleep 2
+        done
+        echo "*********************************$SERVICE Service Stopped..."
+       	elif [ "$SERVICE_STATUS" == INSTALLED ]; then
+       	echo "*********************************$SERVICE Service Stopped..."
+       	fi
+}
+
+startService (){
+       	SERVICE=$1
+       	SERVICE_STATUS=$(getServiceStatus $SERVICE)
+       	echo "*********************************Starting Service $SERVICE ..."
+       	if [ "$SERVICE_STATUS" == INSTALLED ]; then
+        TASKID=$(curl -u admin:BadPass#1 -H "X-Requested-By:ambari" -i -X PUT -d "{\"RequestInfo\": {\"context\": \"Start $SERVICE\"}, \"ServiceInfo\": {\"maintenance_state\" : \"OFF\", \"state\": \"STARTED\"}}" http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/$SERVICE | grep "id" | grep -Po '([0-9]+)')
+
+        echo "*********************************Start $SERVICE TaskID $TASKID"
+        sleep 2
+        LOOPESCAPE="false"
+        until [ "$LOOPESCAPE" == true ]; do
+            TASKSTATUS=$(curl -u admin:BadPass#1 -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/requests/$TASKID | grep "request_status" | grep -Po '([A-Z]+)')
+            if [[ "$TASKSTATUS" == COMPLETED || "$TASKSTATUS" == FAILED ]]; then
+                LOOPESCAPE="true"
+            fi
+            echo "*********************************Start $SERVICE Task Status $TASKSTATUS"
+            sleep 2
+        done
+       	elif [ "$SERVICE_STATUS" == STARTED ]; then
+       	echo "*********************************$SERVICE Service Started..."
+       	fi
+}
+
+getComponentStatus () {
+       	SERVICE=$1
+       	COMPONENT=$2
+       	COMPONENT_STATUS=$(curl -u admin:BadPass#1 -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/$SERVICE/components/$COMPONENT | grep '"state" :' | grep -Po '([A-Z]+)')
+
+       	echo $COMPONENT_STATUS
+}
+
+getHiveServerHost () {
+        HIVESERVER_HOST=$(curl -u admin:BadPass#1 -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/HIVE/components/HIVE_SERVER|grep "host_name"|grep -Po ': "([a-zA-Z0-9\-_!?.]+)'|grep -Po '([a-zA-Z0-9\-_!?.]+)')
+
+        echo $HIVESERVER_HOST
+}
+
+getHiveMetaStoreHost () {
+        HIVE_METASTORE_HOST=$(curl -u admin:BadPass#1 -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/HIVE/components/HIVE_METASTORE|grep "host_name"|grep -Po ': "([a-zA-Z0-9\-_!?.]+)'|grep -Po '([a-zA-Z0-9\-_!?.]+)')
+
+        echo $HIVE_METASTORE_HOST
+}
+
+getStormUIHost () {
+        STORMUI_HOST=$(curl -u admin:BadPass#1 -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/STORM/components/STORM_UI_SERVER|grep "host_name"|grep -Po ': "([a-zA-Z0-9\-_!?.]+)'|grep -Po '([a-zA-Z0-9\-_!?.]+)')
+
+        echo $STORMUI_HOST
+}
+
+getRegistryHost () {
+       	REGISTRY_HOST=$(curl -u admin:BadPass#1 -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/REGISTRY/components/REGISTRY_SERVER |grep "host_name"|grep -Po ': "([a-zA-Z0-9\-_!?.]+)'|grep -Po '([a-zA-Z0-9\-_!?.]+)')
+       	
+       	echo $REGISTRY_HOST
+}
+
+getLivyHost () {
+       	LIVY_HOST=$(curl -u admin:BadPass#1 -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/SPARK2/components/LIVY2_SERVER |grep "host_name"|grep -Po ': "([a-zA-Z0-9\-_!?.]+)'|grep -Po '([a-zA-Z0-9\-_!?.]+)')
+       	
+       	echo $LIVY_HOST
+}
+
+getNameNodeHost () {
+        NAME_NODE=$(curl -u admin:BadPass#1 -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/HDFS/components/NAMENODE|grep "host_name"|grep -Po ': "([a-zA-Z0-9\-_!?.]+)'|grep -Po '([a-zA-Z0-9\-_!?.]+)')
+
+        echo $NAME_NODE
+}
+
+getHiveInteractiveServerHost () {
+        HIVESERVER_INTERACTIVE_HOST=$(curl -u admin:BadPass#1 -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/HIVE/components/HIVE_SERVER_INTERACTIVE|grep "host_name"|grep -Po ': "([a-zA-Z0-9\-_!?.]+)'|grep -Po '([a-zA-Z0-9\-_!?.]+)')
+
+        echo $HIVESERVER_INTERACTIVE_HOST
+}
+
+getDruidBroker () {
+        DRUID_BROKER=$(curl -u admin:BadPass#1 -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/DRUID/components/DRUID_BROKER|grep "host_name"|grep -Po ': "([a-zA-Z0-9\-_!?.]+)'|grep -Po '([a-zA-Z0-9\-_!?.]+)')
+
+        echo $DRUID_BROKER
+}
+
+getKafkaBroker () {
+       	KAFKA_BROKER=$(curl -u admin:BadPass#1 -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/KAFKA/components/KAFKA_BROKER |grep "host_name"|grep -Po ': "([a-zA-Z0-9\-_!?.]+)'|grep -Po '([a-zA-Z0-9\-_!?.]+)')
+       	
+       	echo $KAFKA_BROKER
+}
+
+getAtlasHost () {
+       	ATLAS_HOST=$(curl -u admin:BadPass#1 -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/ATLAS/components/ATLAS_SERVER |grep "host_name"|grep -Po ': "([a-zA-Z0-9\-_!?.]+)'|grep -Po '([a-zA-Z0-9\-_!?.]+)')
+       	
+       	echo $ATLAS_HOST
+}
+
+getNifiHost () {
+       	NIFI_HOST=$(curl -u admin:BadPass#1 -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/NIFI/components/NIFI_MASTER|grep "host_name"|grep -Po ': "([a-zA-Z0-9\-_!?.]+)'|grep -Po '([a-zA-Z0-9\-_!?.]+)')
+
+       	echo $NIFI_HOST
+}
+
+captureEnvironment () {
+	export NIFI_HOST=$(getNifiHost)
+	export NAMENODE_HOST=$(getNameNodeHost)
+	export HIVESERVER_HOST=$(getHiveServerHost)
+	export HIVE_METASTORE_HOST=$(getHiveMetaStoreHost)
+	export HIVE_METASTORE_URI=thrift://$HIVE_METASTORE_HOST:9083
+	export ZK_HOST=$AMBARI_HOST
+	export KAFKA_BROKER=$(getKafkaBroker)
+	export ATLAS_HOST=$(getAtlasHost)
+	export COMETD_HOST=$AMBARI_HOST
+	env
+	echo "export NIFI_HOST=$NIFI_HOST" >> /etc/bashrc
+	echo "export NAMENODE_HOST=$NAMENODE_HOST" >> /etc/bashrc
+	echo "export ZK_HOST=$ZK_HOST" >> /etc/bashrc
+	echo "export KAFKA_BROKER=$KAFKA_BROKER" >> /etc/bashrc
+	echo "export ATLAS_HOST=$ATLAS_HOST" >> /etc/bashrc
+	echo "export HIVE_METASTORE_HOST=$HIVE_METASTORE_HOST" >> /etc/bashrc
+	echo "export HIVE_METASTORE_URI=$HIVE_METASTORE_URI" >> /etc/bashrc
+	echo "export COMETD_HOST=$COMETD_HOST" >> /etc/bashrc
+
+	echo "export NIFI_HOST=$NIFI_HOST" >> ~/.bash_profile
+	echo "export NAMENODE_HOST=$NAMENODE_HOST" >> ~/.bash_profile
+	echo "export ZK_HOST=$ZK_HOST" >> ~/.bash_profile
+	echo "export KAFKA_BROKER=$KAFKA_BROKER" >> ~/.bash_profile
+	echo "export ATLAS_HOST=$ATLAS_HOST" >> ~/.bash_profile
+	echo "export HIVE_METASTORE_HOST=$HIVE_METASTORE_HOST" >> ~/.bash_profile
+	echo "export HIVE_METASTORE_URI=$HIVE_METASTORE_URI" >> ~/.bash_profile
+	echo "export COMETD_HOST=$COMETD_HOST" >> ~/.bash_profile
+
+	. ~/.bash_profile
+}
+
+
+createHiveTables () {    
+  sudo chmod 755 /home/cloudbreak
+  cp /root/twitter_hdp/hiveddl_gcp.sql /home/cloudbreak  
+  sudo -u hive hive -f /home/cloudbreak/hiveddl_gcp.sql 
+}
+
+exec > >(tee -i /home/cloudbreak/demo-install.log)
+exec 2>&1
+
+export ROOT_PATH=~
+echo "*********************************ROOT PATH IS: $ROOT_PATH"
+
+
+export AMBARI_HOST=$(hostname -f)
+echo "*********************************AMABRI HOST IS: $AMBARI_HOST"
+
+export CLUSTER_NAME=$(cat /opt/metadata/cluster.json |jq -r .clusterName)
+echo "*********************************AMABRI HOST IS: $CLUSTER_NAME"
+
+output=`curl -u admin:BadPass#1 -i -H 'X-Requested-By: ambari'  http://${AMBARI_HOST}:8080/api/v1/clusters`
+export CLUSTER_NAME=`echo $output | sed -n 's/.*"cluster_name" : "\([^\"]*\)".*/\1/p'`
+
+if [[ -z $CLUSTER_NAME ]]; then
+        echo "Could not connect to Ambari Server. Please run the install script on the same host where Ambari Server is installed."
+        exit 1
+else
+       	echo "*********************************CLUSTER NAME IS: $CLUSTER_NAME"
+fi
+
+export VERSION=`hdp-select status hadoop-client | sed 's/hadoop-client - \([0-9]\.[0-9]\).*/\1/'`
+export INTVERSION=$(echo $VERSION*10 | bc | grep -Po '([0-9][0-9])')
+echo "*********************************HDP VERSION IS: $VERSION"
+
+export HADOOP_USER_NAME=hdfs
+echo "*********************************HADOOP_USER_NAME set to HDFS"
+
+#echo "********************************* Capturing Service Endpoint in the Environment"
+#captureEnvironment
+
+
+echo "********************************* Creating HIVE Tables"
+createHiveTables
